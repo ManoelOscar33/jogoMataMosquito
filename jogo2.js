33e723ad4fdeb561d6bb5c5d51ebec3e47fba9f7
Manoel Oscar <manoel.oscar@hotmail.com>
2022-03-25 20:16:11 -0300

    Primeiro commit

---
+++ jogo2.js
@@ -0,0 +1,121 @@
+var altura = 0;
+var largura = 0;
+var vidas = 1;
+var tempo = 15;
+
+var criaMosquitoTempo = 1500;
+
+var nivel = window.location.search;
+nivel = nivel.replace('?', '');
+
+if (nivel === 'normal') {
+	//1500
+	criaMosquitoTempo = 1500;
+} else if (nivel === 'dificil') {
+	//1000
+	criaMosquitoTempo = 1000;
+} else if (nivel === 'chucknorris') {
+	//750
+	criaMosquitoTempo = 750;
+}
+
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight;
+	largura = window.innerWidth;
+
+	console.log(largura, altura);
+}
+
+ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1;
+
+	if (tempo < 0) {
+		clearInterval(cronometro);
+		clearInterval(criaMosquito);
+		window.location.href = 'vitoria.html';
+	} else if (tempo >= 0) {
+		document.getElementById('cronometro').innerHTML = tempo;
+	} 
+	
+}, 1000);
+
+function posicaoRandomica() {
+	// Remover o mosquito anterior (caso ele exista)
+
+	if (document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove();
+
+		if (vidas > 3) {
+			
+			window.location.href = 'fim_de_jogo.html';
+		} else if (vidas <= 3) {
+			document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png';
+			vidas++
+		}
+
+	}
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90;
+	var posicaoY = Math.floor(Math.random() * altura)- 90;
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX;
+	posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+	console.log(posicaoX, posicaoY);
+
+	// Criar o elemento html
+	var mosquito = document.createElement('img');
+	mosquito.src = 'imagens/mosquito.png';
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+	mosquito.style.left = posicaoX + 'px';
+	mosquito.style.top = posicaoY + 'px';
+	mosquito.style.position = 'absolute';
+	mosquito.id = 'mosquito';
+	mosquito.onclick = function () {
+		this.remove();
+	}
+
+
+	document.body.appendChild(mosquito);
+
+	//console.log(tamanhoAleatorio());
+
+	//console.log(ladoAleatorio());
+
+}
+
+function tamanhoAleatorio () {
+	var classe = Math.floor(Math.random() * 3);
+	//console.log(classe);
+
+	if ( classe === 0) {
+		return 'mosquito1';
+	} 
+
+	if ( classe === 1) {
+		return 'mosquito2';
+	}
+
+	if ( classe === 2) {
+		return 'mosquito3';
+	} 
+}
+
+function ladoAleatorio () {
+	var classe = Math.floor(Math.random() * 2);
+	//console.log(classe);
+
+	if ( classe === 0) {
+		return 'ladoA';
+	}
+
+	if ( classe === 1) {
+		return 'ladoB';
+	}
+}
+
+
+
